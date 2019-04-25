6f609810cbe8959505115e949c5a2e4d516bd2f4
Lucas Ferreira <lucas.182.ferreira@hotmail.com>
2019-04-24 23:04:23 -0300

    Adding complex objects

--- src/index.js
+++ src/index.js
@@ -5,14 +5,45 @@ const { GraphQLServer } = require('graphql-yoga');
 const typeDefs = `
     type Query {
         info: String!
+        feed: [Link!]!
+    }
+    
+    type Link {
+        id: ID!
+        description: String!
+        url: String!
     }`
 
+
+
 // implementation
+let links = [
+    {
+        id: 'link-0',
+        url: 'www.howtographql.com',
+        description: 'Fullstack tutorial for GraphQL'
+    },
+    {
+        id: 'link-1',
+        url: 'www.google.com',
+        description: 'Google'
+    }
+]
+
 const resolvers = { 
     Query: { 
-        info: () => `This is an GraphQL API`
+        info: () => `This is an GraphQL API`,
+        feed: () => links
+    },
+    Link: {
+        id: (parent) => parent.id,
+        description: (parent) => parent.description,
+        url: (parent) => parent.url
     }
 }
+
+
+
 
 const server = new GraphQLServer({
     typeDefs,
